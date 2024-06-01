f43ff5d18fb10e1d5a11a932029f220e93d19eb0
90duse <duseibnuduse1212@gmail.com>
2024-06-01 14:01:01 +0300

    added userinformation class

---
+++ lib/models/userInformation.dart
@@ -0,0 +1,12 @@
+class Userinformation {
+  String name;
+  int number;
+  double account_balance;
+  int id;
+
+  Userinformation(
+      {required this.id,
+      required this.name,
+      required this.account_balance,
+      required this.number});
+}
