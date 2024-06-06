c4b30272c544f52ffbbe889f8444b817e4153d05
90duse <duseibnuduse1212@gmail.com>
2024-06-06 11:00:19 +0300

    added some update

--- lib/pages/evoucher.dart
+++ lib/pages/evoucher.dart
@@ -5,6 +5,7 @@ import '../models/commonfunctions.dart';
 TextEditingController dooroEvoucherController = TextEditingController();
 TextEditingController kushubolacagController = TextEditingController();
 TextEditingController hubilacagtaController = TextEditingController();
+TextEditingController ugushubqofkaleController = TextEditingController();
 
 class Evoucher {
   Future<dynamic> itusEvoucher(BuildContext context) async {
@@ -243,6 +244,7 @@ class Evoucher {
   // ugu shub qof kale operation
 
   Future ugushubqofkale(BuildContext context) async {
+    final numberka;
     await showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -254,12 +256,15 @@ class Evoucher {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                const ListBody(
+                ListBody(
                   children: [
-                    Text(
+                    const Text(
                       '-ZAAD SHILING-',
                     ),
-                    Text('Warbixin kooban ayaa laguu soo dirayaa dhawaan'),
+                    const Text('Geli Numberka aad ugu shubaysid'),
+                    TextFormField(
+                      controller: ugushubqofkaleController,
+                    ),
                   ],
                 ),
                 Row(
@@ -271,6 +276,17 @@ class Evoucher {
                         Navigator.of(context).pop();
                       },
                     ),
+                    TextButton(
+                      child: const Text('Send'),
+                      onPressed: () {
+                        final ugushub = ugushubqofkaleController.text;
+                        if (ugushub.isNotEmpty) {
+                          final numberka = ugushubqofkaleController.text;
+                          hubi(context, numberka);
+                        }
+                        Navigator.of(context).pop();
+                      },
+                    ),
                   ],
                 )
               ],
@@ -279,6 +295,68 @@ class Evoucher {
         );
       },
     );
+    numberka = ugushubqofkaleController.text;
+    return numberka;
+  }
+
+  Future<dynamic> hubi(BuildContext context, dynamic numberka) async {
+    dynamic lacagtalagushubanayo = numberka;
+    await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+              backgroundColor: Colors.white,
+              surfaceTintColor: Colors.white,
+              title: const Text('Send instruction'),
+              content: SingleChildScrollView(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  ListBody(
+                    children: [
+                      Text(
+                        'Ma Hubtaa in aad ku shubanaysid $numberka oo evoucher ah',
+                        textAlign: TextAlign.left,
+                      ),
+                      const Text('1. Haa'),
+                      const Text('2. maya'),
+                      TextFormField(
+                        controller: hubilacagtaController,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      TextButton(
+                        child: const Text('Close'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      const Text("|"),
+                      TextButton(
+                        child: const Text('SEND'),
+                        onPressed: () {
+                          final hubiController = hubilacagtaController.text;
+                          if (hubiController.isNotEmpty &&
+                              hubiController == '1') {
+                            Navigator.of(context).pop();
+                            lacagAyaadkushubatay(context, lacagta);
+
+                            setState(() {
+                              hubilacagtaController.clear();
+                            });
+                          } else {
+                            displaymessage('message');
+                          }
+                        },
+                      ),
+                    ],
+                  )
+                ],
+              )));
+        });
   }
 
   void setState(Null Function() param0) {
