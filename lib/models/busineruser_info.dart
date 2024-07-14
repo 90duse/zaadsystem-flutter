ba4d5c60e88e9a2127b017f673eb851c9ab0db64
90duse <duseibnuduse1212@gmail.com>
2024-07-14 20:47:47 +0300

    added some update

--- lib/models/busineruser_info.dart
+++ lib/models/busineruser_info.dart
@@ -1,8 +1,8 @@
 class Businersuserinformation {
-  String name = 'Dhoolayare Hotel';
-  String firstname = 'Dhoolayre';
+  String name = 'Hotel Hargeisa';
+  String firstname = 'Hargeysa';
   String number = '500400';
   num accountbalance = 1000;
   int id = 1309;
-  String password = '1000';
+  //String password = '1000';
 }
